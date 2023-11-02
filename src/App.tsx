5872fba516c8dd6dd5b810607ad7b5a91b987a0f
bsextion <bsextion@gmail.com>
2023-11-01 21:14:36 -0500

    add css classes and form

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-
+import { useState } from 'react';
+import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
-const todos = [
-  new Todo('Learn React'),
-  new Todo('Learn Typescript'),
-]
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (text: string) => {
+    const newTodo = new Todo(text);
+    setTodos((prev) => {
+      return [newTodo, ...prev];
+    });
+  };
+
+  const removeHandler = (todoId: string) => {
+    setTodos((prev) => {
+      return prev.filter((todo) => todo.id != todoId);
+    })
+  }
+
   return (
     <div>
-      <Todos items={todos}/>
+      <NewTodo onAddTodo={addTodoHandler} />
+      <Todos items={todos} onRemove={removeHandler}/>
     </div>
   );
 }
